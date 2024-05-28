58d055cbaeb8c1703ed0b94d4e31ced2d68d8d17
brookpsilver <93788937+brookpsilver@users.noreply.github.com>
2024-05-28 16:25:59 -0700

    Addedsimilar loop to assign names to warm springs list containing expansion tables. Do we need to do this?

--- global.R
+++ global.R
@@ -2,3 +2,4 @@ library(shiny)
 source("./data/WarmSprings/massage.R") # Remove this when app launches (or done with WS tab)
 load("data/WarmSprings/WSdata.Rdata") # Warm Springs plotting data
 for(i in 1:length(L_Plot_cum)) assign(names(L_Plot_cum)[i], L_Plot_cum[[i]])
+for(i in 1:length(L_ws_expansion)) assign(names(L_ws_expansion)[i], L_ws_expansion[[i]])
